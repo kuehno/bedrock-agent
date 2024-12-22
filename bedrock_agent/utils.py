c5179aabb0ca4b131b61daebc8949e4164638196
Niklas Kuehn <niklaskuehn@gmx.de>
2024-12-22 17:01:54 +0100

    added example for local rag using pgvector

--- bedrock_agent/utils.py
+++ bedrock_agent/utils.py
@@ -1,4 +1,23 @@
 import boto3
+import json
 
 def get_boto_session(region_name='us-east-1', *args, **kwargs):
     return boto3.Session(region_name=region_name, *args, **kwargs)
+
+def get_embedding(input_text: str, session: boto3.Session = None, model_id: str = "amazon.titan-embed-text-v2:0", region_name: str = "us-east-1", dimensions: int = 256):
+    if not session:
+        session = get_boto_session(region_name=region_name)
+        
+    client = session.client("bedrock-runtime", region_name=region_name)
+    
+    native_request = {"inputText": input_text, "dimensions": dimensions}
+    request = json.dumps(native_request)
+    
+    response = client.invoke_model(
+        modelId=model_id, 
+        body=request
+        )
+    
+    model_response = json.loads(response["body"].read())
+    embedding = model_response["embedding"]
+    return embedding
---
+++ cookbook/local_rag_pgvector/init_scripts/init.sql
@@ -0,0 +1,2 @@
+CREATE EXTENSION vector;
+CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3));
